b83e968206fe958ab49274004fdbaea3dd33e555
DOLCEY MENDOZA <DOLCEY MENDOZA@DESKTOP-R4U1A54>
2022-04-08 15:22:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Stage1Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.Write("Digite una cadena a limpiar:");
+            Console.WriteLine("Cadena limpia: " + AvoidSpecialCharacters(Console.ReadLine()));
+        }
+
+
+        static string AvoidSpecialCharacters(string text)
+        {
+            string replacedText= Regex.Replace(text, @"[^\w\-\s]", "");
+            return replacedText == String.Empty ? "NA":replacedText;
+        }
+    }
+}
